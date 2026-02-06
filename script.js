67b40ba671249f2516fcc95c371a38c134c45ddd
ajtoriani <anajuliatoriani@gmail.com>
2026-02-06 16:24:31 -0300

    Primeira Versão ST

---
+++ script.js
@@ -0,0 +1,114 @@
+﻿// GSAP - CDN
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
+
+// SCROLL SUAVE
+ScrollSmoother.create({
+    wrapper: "#smooth-wrapper",
+    content: "#smooth-content",
+    smooth: 1.5,
+    normalizeScroll: true,
+    effects: true
+});
+
+function animacaoPagina() {
+    // ANIMAÇÕES HERO
+    gsap.from(".hero", {
+        opacity: 0,
+        duration: 1
+    });
+
+    gsap.from(".hero picture:nth-of-type(2)", {
+        y: 60,
+        duration: 1
+    });
+
+    gsap.from(".hero picture:nth-of-type(1)", {
+        y: -60,
+        duration: 1
+    });
+
+    // ANIMAÇÕES CARDS
+    gsap.from(".card", {
+        opacity: 0,
+        filter: "blur(10px)",
+        stagger: 0.3,
+        scrollTrigger: {
+            trigger: ".cards",
+            start: "0% 80%",
+            end: "100% 70%",
+            scrub: true
+        }
+    });
+
+    gsap.from(".secaoAgradecimentos ul li", {
+        opacity: 0,
+        x: 60,
+        filter: "blur(10px)",
+        stagger: 0.25,
+        scrollTrigger: {
+            trigger: ".secaoAgradecimentos",
+            start: "top 85%",
+            end: "bottom 40%",
+            scrub: 1.5
+        }
+    });
+
+    // ANIMAÇÕES FOOTER
+    gsap.from("footer", {
+        y: "-30%",
+        immediateRender: false,
+        scrollTrigger: {
+            trigger: "footer",
+            scrub: true,
+            invalidateOnRefresh: true,
+            end: "100% 100%"
+        }
+    });
+
+    // SELECIONANDO TODOS OS ELEMENTOS .textoAnimado
+    const textosAnimados = document.querySelectorAll(".textoAnimado");
+
+    // ANIMANDO CADA ELEMENTO .textoAnimado
+    textosAnimados.forEach((animacaoUnica) => {
+        const split = SplitText.create(animacaoUnica, {
+            type: "lines, words, chars",
+            mask: "lines"
+        });
+
+        gsap.from(split.chars, {
+            y: 40,
+            opacity: 0,
+            duration: 0.3,
+            stagger: 0.03,
+            scrollTrigger: {
+                trigger: animacaoUnica
+            }
+        });
+    });
+}
+
+// PRELOADER ANIMAÇÃO
+const tl = gsap.timeline({
+    onComplete() {
+        animacaoPagina();
+        gsap.to("#preloader", {
+            opacity: 0,
+            duration: 0.4,
+            pointerEvents: "none",
+            onComplete: () => {
+                document.getElementById("preloader").classList.add("hidden");
+            }
+        });
+    }
+});
+
+tl.to("#preloader path", {
+    duration: 2,
+    strokeDashoffset: 0
+});
+
+tl.to("#preloader path", {
+    fill: "rgba(168, 19, 19)",
+    duration: 1,
+    strokeDashoffset: 0
+});
